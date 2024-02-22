9661352dca1773a72cac7476780c0080da5a79f4
Euwamahoro <e.uwamahoro@alustudent.com>
2024-02-22 14:27:04 +0200

    This is Enock

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 """
 This is module 8-cities_by_state
-In this module we combine flask with sqlAlchemy for the first time
-Run this script from AirBnB_v2 directory for imports
+In this module, we combine Flask with SQLAlchemy for the first time.
+Run this script from the AirBnB_v2 directory for imports.
 """
 from models import storage
-from models.base_model import Base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from os import getenv
-from models.user import User
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/states/')
 @app.route('/states/<id_d>')
 def cities_by_states(id_d="all"):
+    """
+    Display a web page with a list of states or cities in a specific state.
+
+    Parameters:
+        id_d (str): State ID or 'all' to display all states.
+
+    Returns:
+        str: Rendered HTML template.
+    """
     states = storage.all("State")
     if id_d == "all":
         return render_template("9-states.html", state="all",
@@ -53,8 +52,3 @@ def close_session(exception):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
-#    cities_by_states()
-#    print("------------")
-#    cities_by_states('421a55f4-7d82-47d9-b54c-a76916479556')
-#    print("------------")
-#    cities_by_states("hi")
