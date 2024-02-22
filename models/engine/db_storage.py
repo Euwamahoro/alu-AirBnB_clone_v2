28962ecffd1f857f614762ea4257af2466ecdeb3
Euwamahoro <e.uwamahoro@alustudent.com>
2024-02-23 00:41:50 +0200

    This is Enock

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,184 +1,84 @@
 #!/usr/bin/python3
-"""
-This is the db_storage module.
-This module deals with storing and retrieving data from a MySQL database.
-This module contains one class DBStorage.
-"""
-
-from models import storage
-from models.amenity import Amenity
+""" new class for sqlAlchemy """
+from os import getenv
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
+from models.state import State
 from models.city import City
+from models.user import User
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models.user import User
-from os import getenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from flask import Flask, render_template
+from models.amenity import Amenity
 
-app = Flask(__name__)
 
 class DBStorage:
-    """
-    class DBStorage
-    Save and retrieve data from a MySQL database using SQLAlchemy ORM
-
-    **class attributes**
-       __engine: private, SQLAlchemy engine
-       __session: private, scoped session
-
-    instance attributes:
-       __models_available: private, dictionary of <string> <class>
-    """
+    """ create tables in environmental"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """
-        Initializes engine
-        """
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            getenv('HBNB_MYSQL_USER'),
-            getenv('HBNB_MYSQL_PWD'),
-            getenv('HBNB_MYSQL_HOST'),
-            getenv('HBNB_MYSQL_DB')))
-        self.__models_available = {"User": User,
-                                   "Amenity": Amenity, "City": City,
-                                   "Place": Place, "Review": Review,
-                                   "State": State}
-        if getenv('HBNB_MYSQL_ENV', 'not') == 'test':
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
+
+        if env == "test":
             Base.metadata.drop_all(self.__engine)
 
-        # Initialize a scoped session
-        self.__session = scoped_session(sessionmaker(bind=self.__engine,
-                                                     expire_on_commit=False))
-
     def all(self, cls=None):
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
         """
-        Returns a dictionary of all the class objects
-        """
-        orm_objects = {}
+        dic = {}
         if cls:
-            if cls in self.__models_available:
-                for k in self.__session.query(
-                        self.__models_available.get(cls)):
-                    orm_objects[k.__dict__['id']] = k
+            if isinstance(cls, str):
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                dic[key] = elem
         else:
-            for i in self.__models_available.values():
-                j = self.__session.query(i).all()
-                if j:
-                    for k in j:
-                        orm_objects[k.__dict__['id']] = k
-        return orm_objects
+            lista = [State, City, User, Place, Review, Amenity]
+            for clase in lista:
+                query = self.__session.query(clase)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__, elem.id)
+                    dic[key] = elem
+        return (dic)
 
     def new(self, obj):
-        """
-        Adds a new obj to the session
+        """add a new element in the table
         """
         self.__session.add(obj)
 
     def save(self):
-        """
-        Saves the objects from the current session
+        """save changes
         """
         self.__session.commit()
 
     def delete(self, obj=None):
+        """delete an element in the table
         """
-        Deletes an object from the current session
-        """
-        if obj is not None:
-            self.__session.delete(obj)
-            self.__session.commit()
+        if obj:
+            self.session.delete(obj)
 
     def reload(self):
-        """
-        WARNING!!!! I'm not sure if Base.metadata.create_all needs to
-        be in the init method
+        """configuration
         """
         Base.metadata.create_all(self.__engine)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
+        self.__session = Session()
 
     def close(self):
+        """ calls remove()
         """
-        Close a session
-        """
-        self.__session.remove()
-
-    def get(self, cls, id_):
-        """
-        Retrieve one object
-
-        Arguments:
-            cls: string representing a class name
-            id_: string representing the object id, primary key
-
-        Return:
-           object of cls and id passed in argument or None
-        """
-        if (cls not in self.__models_available) or (id_ is None):
-            return None
-        return self.__session.query(
-                self.__models_available[cls]).get(id_)
-
-    def count(self, cls=None):
-        """
-        Number of objects in a certain class
-
-        Arguments:
-            cls: optional, string representing a class name (default None)
-
-        Return:
-            number of objects in that class or in total
-            -1 if the argument is not valid
-        """
-        if cls is None:
-            total = 0
-            for v in self.__models_available.values():
-                total += self.__session.query(v).count()
-            return total
-        if cls in self.__models_available.keys():
-            return self.__session.query(self.__models_available[cls]).count()
-        return -1
-
-@app.route('/states/')
-@app.route('/states/<id_d>')
-def cities_by_states(id_d="all"):
-    """
-    Display a web page with a list of states or cities in a specific state.
-
-    Parameters:
-        id_d (str): State ID or 'all' to display all states.
-
-    Returns:
-        str: Rendered HTML template.
-    """
-    states = storage.all("State")
-    if id_d == "all":
-        return render_template("9-states.html", state="all",
-                               Query_name="States",
-                               states=states.values())
-    else:
-        flag = False
-        for k, v in states.items():
-            if k == id_d:
-                flag = True
-                break
-        if flag:
-            result = v.cities
-            return render_template("9-states.html", state="1",
-                                   Query_name="State: {}".format(v.name),
-                                   states=result)
-        else:
-            return render_template("9-states.html", state="",
-                                   Query_name="Not found!",
-                                   states=states)
-
-@app.teardown_appcontext
-def close_session(exception):
-    """Remove the db session or save file"""
-    storage.close()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+        self.__session.close()
