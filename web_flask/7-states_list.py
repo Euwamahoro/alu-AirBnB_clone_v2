a36052b18fb945fa23fa155db7472f816b660f6b
Euwamahoro <e.uwamahoro@alustudent.com>
2024-02-23 11:51:12 +0200

    This is Enock

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-This is module 7-states_list
-In this module we combine flask with sqlAlchemy for the first time
-Run this script from AirBnB_v2 directory for imports
+This is module 7-states_list.
+
+In this module, we combine Flask with SQLAlchemy for the first time.
+Run this script from AirBnB_v2 directory for imports.
 """
 from models import storage
 from models.base_model import Base
@@ -22,7 +23,7 @@ app = Flask(__name__)
 
 @app.route('/states_list/')
 def list_states():
-    """list all states in a db"""
+    """List all states in a database."""
     states = storage.all("State").values()
     return render_template("7-states_list.html",
                            Query_name="States", states=states)
@@ -30,9 +31,9 @@ def list_states():
 
 @app.teardown_appcontext
 def close_session(exception):
-    """remove the session to see what happened"""
+    """Remove the session to see what happened."""
     storage.close()
 
 
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port="5300")
+    app.run(host="0.0.0.0", port="5300")
