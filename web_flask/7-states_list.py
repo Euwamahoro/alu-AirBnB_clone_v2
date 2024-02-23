b14538c09f91be64d766f365fcd68f647b2566d2
Euwamahoro <e.uwamahoro@alustudent.com>
2024-02-23 11:37:44 +0200

    This is Enock

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,40 +1,38 @@
 #!/usr/bin/python3
 """
-Starts a Flask web application.
+This is module 7-states_list
+In this module we combine flask with sqlAlchemy for the first time
+Run this script from AirBnB_v2 directory for imports
 """
-
-
-from models import storage #This import storage from models
-from models.state import State #This one import state from models.state
-from flask import Flask, render_template # This one import Flask from flask
-
-
+from models import storage
+from models.base_model import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from os import getenv
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states():
-    """
-    Display a HTML page with a list of states.
-
-    Returns:
-        HTML page displaying a list of states.
-    """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+@app.route('/states_list/')
+def list_states():
+    """list all states in a db"""
+    states = storage.all("State").values()
+    return render_template("7-states_list.html",
+                           Query_name="States", states=states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """
-    Remove the current SQLAlchemy Session after each request.
-
-    Args:
-        exception: Exception that might have occurred during the request.
-    """
+def close_session(exception):
+    """remove the session to see what happened"""
     storage.close()
 
-#Test the function
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+        app.run(host="0.0.0.0", port="5300")
