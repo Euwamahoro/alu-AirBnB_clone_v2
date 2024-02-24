bc484cc6ddcfe526b9f7c281aa9a846b183da290
Euwamahoro <e.uwamahoro@alustudent.com>
2024-02-24 23:47:27 +0200

    This is Enock

--- models/state.py
+++ models/state.py
@@ -1,39 +1,28 @@
 #!/usr/bin/python3
-import models
-from models.base_model import BaseModel, Base, Table, Column, String
-from sqlalchemy.orm import relationship, backref
-from os import getenv
 """
-state module
-    contain
-        State class
+State Module
 """
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import environ
 
 class State(BaseModel, Base):
-    """
-    This is the State class
-    """
-    if getenv('HBNB_TYPE_STORAGE', 'fs') == 'db':
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="all, delete, delete-orphan")
+    """State class"""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete-orphan",
+                              passive_deletes=True, single_parent=True)
+
     else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """
-        initializes from BaseModel Class
-        """
-        super(State, self).__init__(*args, **kwargs)
-
-    if getenv('HBNB_TYPE_STORAGE', 'fs') != 'db':
         @property
         def cities(self):
-            """
-            returns all cities in a State
-            """
-            all_cities = models.storage.all("City").values()
-            result = [city for city in all_cities if city.state_id == self.id]
-            return result
+            """Getter method to return the list of City objects linked to the current State"""
+            cities_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
