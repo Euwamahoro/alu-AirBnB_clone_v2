af0c7c24af851fe88b2e59e3a553a9fb3af04ffb
Euwamahoro <e.uwamahoro@alustudent.com>
2024-02-24 22:22:55 +0200

    This is Enock

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -9,26 +9,33 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_hbnb():
     """Flask hello world."""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb')
 def hbnb():
     """Add a path to the URL."""
     return "HBNB"
 
+
 @app.route('/c/<text>')
 def c_text(text):
     """Make a simple variable rule."""
     return "C {}".format(text.replace("_", " "))
+
+
 
 @app.route('/python/', defaults={'text': "is cool"})
 @app.route('/python/<text>')
 def python_text(text):
     """Give a rule a default value."""
     return "Python {}".format(text.replace("_", " "))
+
+
 
 @app.route('/number/<int:n>')
 def number_route(n):
